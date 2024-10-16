abad0ba43e0c9d0c23dcd5ac4608eca1bf262ef3
Randal <randal.t.meyer@gmail.com>
2024-10-16 09:32:07 -0700

    add test case with decimal value

--- test/number/addCommas.mjs
+++ test/number/addCommas.mjs
@@ -3,4 +3,5 @@ import { addCommas, assert, runTests } from "../../build/index.js";
 runTests(async function test_addCommas() {
 	assert("12,345", addCommas, 12345);
 	assert("12,345,678", addCommas, 12345678);
+	assert("12,345,678.09", addCommas, 12345678.09);
 }, true);
