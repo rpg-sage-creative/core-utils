973d5e2969815cbcbc5d9b198ff2d92d6aa3152d
Randal <randal.t.meyer@gmail.com>
2024-10-17 15:27:24 -0700

    add sum function to number

--- build/number/index.js
+++ build/number/index.js
@@ -4,4 +4,5 @@ export * from "./getNumberRegex.js";
 export * from "./nth.js";
 export * from "./parseNumeric.js";
 export * from "./round.js";
+export * from "./sum.js";
 export * from "./toScriptedNumber.js";
---
+++ test/number/sum.mjs
@@ -0,0 +1,6 @@
+import { assert, runTests, sum } from "../../build/index.js";
+
+runTests(async function test_sum() {
+	assert(-1, sum, [1, 2, -3, -4, 3]);
+	assert(10, sum, [1, 2, 3, 4]);
+}, true);
