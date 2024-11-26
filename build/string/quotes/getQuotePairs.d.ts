414bb53e92819616ab5925c63735210054c65a19
Randal <randal.t.meyer@gmail.com>
2024-11-26 08:40:38 -0800

    expand docs/notes about the different types and literals

--- build/string/quotes/getQuotePairs.d.ts
+++ build/string/quotes/getQuotePairs.d.ts
@@ -6,9 +6,11 @@
  *
  * "strict" limits to pure "double" and 'single' quotes
  * "fancy" limits to pure "double" and 'single' and curly “double” and ‘single’ quotes
+ * "extended" limits to pure "double" and 'single' and curly “double” and ‘single’ quotes as well as „German“ and „Polish” double quotes
  *
  * "double-strict" limits to pure "double" quotes
  * "double-fancy" limits to pure "double" and curly “double” quotes
+ * "double-extended" limits to pure "double" and curly “double” quotes as well as „German“ and „Polish” double quotes
  *
  * "single-strict" limits to pure 'single' quotes
 */
