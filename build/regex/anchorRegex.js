5b9689acdf669989bc3b3758e7fbd1cc66f0178a
Randal <randal.t.meyer@gmail.com>
2024-11-27 13:25:02 -0800

    improve anchorRegex reliability

--- build/regex/anchorRegex.js
+++ build/regex/anchorRegex.js
@@ -1,8 +1,5 @@
-import { rewrite } from "regex";
+import { regex } from "regex";
 import { copyFlags } from "./internal/copyFlags.js";
 export function anchorRegex(regexp) {
-    const source = `^(?:${regexp.source})$`;
-    const options = { flags: copyFlags(regexp) };
-    const { expression, flags } = rewrite(source, options);
-    return new RegExp(expression, flags);
+    return regex(copyFlags(regexp)) `^${regexp}$`;
 }
--- src/regex/anchorRegex.ts
+++ src/regex/anchorRegex.ts
@@ -1,4 +1,4 @@
-import { rewrite } from "regex";
+import { regex } from "regex";
 import { copyFlags } from "./internal/copyFlags.js";
 
 /**
@@ -6,8 +6,5 @@ import { copyFlags } from "./internal/copyFlags.js";
  * Because this uses the regex library, the resulting RegExp will include the "u" flag.
  */
 export function anchorRegex(regexp: RegExp): RegExp {
-	const source = `^(?:${regexp.source})$`;
-	const options = { flags:copyFlags(regexp) };
-	const { expression, flags } = rewrite(source, options);
-	return new RegExp(expression, flags);
+	return regex(copyFlags(regexp))`^${regexp}$`;
 }
