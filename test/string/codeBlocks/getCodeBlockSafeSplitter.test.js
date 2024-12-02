a3035b8c54bb1062dc07d4a7612447a7cfae4ffb
Randal <randal.t.meyer@gmail.com>
2024-12-01 21:03:09 -0800

    rewrite test data

--- test/string/codeBlocks/getCodeBlockSafeSplitter.test.js
+++ test/string/codeBlocks/getCodeBlockSafeSplitter.test.js
@@ -7,31 +7,25 @@ describe("string", () => {
 		// const splitter = getCodeBlockSafeSplitter(/\n/);
 		const splitter = getCodeBlockSafeSplitter("\n");
 
-		/** @type {[string, number | string[], string[] | undefined][]} */
-		const strings = [
-			["first\nsecond\nthird\nfourth\nfifth", 3, ["first","second","third"]],
-			["\n\n\n\n", ["","","","",""]],
-			["only", ["only"]],
-			["\nsecond", ["","second"]],
-			["first\nsecond",["first","second"]],
-			["first\n\nthird",["first","","third"]],
-			["start `left\nright` end", ["start `left\nright` end"]],
-			["first\n`left\nright`\nthird",["first","`left\nright`","third"]],
-			["first\n``left\nright``\nthird",["first","``left\nright``","third"]],
-			["first\n```left\nright```\nthird",["first","```left\nright```","third"]],
-			["first\n``left`\nright``\nthird",["first","``left`\nright``","third"]],
-			["fir``st\n`left\nright``\nthird",["fir``st\n`left\nright``","third"]],
-			[";\n`jk\nl;kj\n`\nj`\nkj;`\njk`\njk;`\nk;lj`k\n;`;\njkl`\n",[";","`jk\nl;kj\n`","j`\nkj;`","jk`\njk;`","k;lj`k\n;`;","jkl`",""]],
-			["pc::Well?\n`hoobla::What?`\ngm::...and `there\nthey` waited ...",["pc::Well?","`hoobla::What?`","gm::...and `there\nthey` waited ..."]],
+		const tests = [
+			{ input:"first\nsecond\nthird\nfourth\nfifth", limit:3, expected:["first","second","third"] },
+			{ input:"\n\n\n\n", expected:["","","","",""] },
+			{ input:"only", expected:["only"] },
+			{ input:"\nsecond", expected:["","second"] },
+			{ input:"first\nsecond", expected:["first","second"] },
+			{ input:"first\n\nthird", expected:["first","","third"] },
+			{ input:"start `left\nright` end", expected:["start `left\nright` end"] },
+			{ input:"first\n`left\nright`\nthird", expected:["first","`left\nright`","third"] },
+			{ input:"first\n``left\nright``\nthird", expected:["first","``left\nright``","third"] },
+			{ input:"first\n```left\nright```\nthird", expected:["first","```left\nright```","third"] },
+			{ input:"first\n``left`\nright``\nthird", expected:["first","``left`\nright``","third"] },
+			{ input:"fir``st\n`left\nright``\nthird", expected:["fir``st\n`left\nright``","third"] },
+			{ input:";\n`jk\nl;kj\n`\nj`\nkj;`\njk`\njk;`\nk;lj`k\n;`;\njkl`\n", expected:[";","`jk\nl;kj\n`","j`\nkj;`","jk`\njk;`","k;lj`k\n;`;","jkl`",""] },
+			{ input:"pc::Well?\n`hoobla::What?`\ngm::...and `there\nthey` waited ...", expected:["pc::Well?","`hoobla::What?`","gm::...and `there\nthey` waited ..."] },
 		];
-		strings.forEach(([raw, limitOrExpected, expectedOrUndefined]) => {
-			const limit = typeof(limitOrExpected) === "number" ? limitOrExpected : undefined;
-			/** @type {string[]} */
-			const expected = expectedOrUndefined ?? limitOrExpected ?? [];
-			// const actual = raw.split(splitter, limit);
-			// assert(expected.length === actual.length && expected.every((s, i) => actual[i] === s), `"${raw.replace(/\n/g, "\\n")}"${limit===undefined?"":`(${limit})`} expected ${JSON.stringify(expected)} got ${JSON.stringify(actual)}`)
-			test(`raw.split(${toString(splitter)}, ${toString(limit)}) === ${toString(expected)}`, () => {
-				expect(raw.split(splitter, limit)).toStrictEqual(expected);
+		tests.forEach(({ input, limit, expected }) => {
+			test(`${toString(input)}.split(${toString(splitter)}, ${toString(limit)}) === ${toString(expected)}`, () => {
+				expect(input.split(splitter, limit)).toStrictEqual(expected);
 			});
 		});
 
