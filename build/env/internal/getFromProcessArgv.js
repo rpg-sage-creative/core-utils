9042ffe7aac5184bab2a8b1afa03a017f0bea127
Randal <randal.t.meyer@gmail.com>
2024-12-06 03:21:34 -0800

    reuse parseKeyValueArg

--- build/env/internal/getFromProcessArgv.js
+++ build/env/internal/getFromProcessArgv.js
@@ -1,19 +1,9 @@
-function parseKeyValueArg(input, key) {
-    const regex = new RegExp(`^${key}=("[^"]+"|'[^']+'|\\S+)$`, "i");
-    if (regex.test(input)) {
-        const value = input.slice(input.indexOf("=") + 1);
-        if (/^("[^"]+"|'[^']+')$/.test(value)) {
-            return value.slice(1, -1).trim();
-        }
-        return value;
-    }
-    return undefined;
-}
+import { parseKeyValueArg } from "../../args/parseKeyValueArg.js";
 export function getFromProcessArgv(key) {
     for (const arg of process.argv) {
-        const value = parseKeyValueArg(arg, key);
-        if (value) {
-            return value;
+        const pair = parseKeyValueArg(arg, { key });
+        if (pair?.value) {
+            return pair.value;
         }
     }
     return undefined;
