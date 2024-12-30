6619ee4cbdd00f1d52ae23727308f9d34015f4cb
Randal <randal.t.meyer@gmail.com>
2024-12-30 11:40:01 -0800

    implement deepFreeze

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export * from "./enums/index.js";
 export * from "./typeGuards/index.js";
 export * from "./applyMixins.js";
+export * from "./deepFreeze.js";
 export * from "./generics.js";
 export * from "./Matcher.js";
 export * from "./omit.js";
