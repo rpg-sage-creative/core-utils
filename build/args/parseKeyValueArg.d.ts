704879126dc004fb0db6fe8bcac8946243302dd1
Randal <randal.t.meyer@gmail.com>
2025-03-03 17:46:08 -0800

    update typing

--- build/args/parseKeyValueArg.d.ts
+++ build/args/parseKeyValueArg.d.ts
@@ -4,4 +4,4 @@ import type { RegExpKeyValueArgOptions } from "./getKeyValueArgRegex.js";
  * Returns KeyValueArg if the input is a valid key/value pairing, undefined otherwise.
  * If key is given then the key must match the valid key/value pair.
  */
-export declare function parseKeyValueArg(input: string, options?: RegExpKeyValueArgOptions): KeyValueArg | undefined;
+export declare function parseKeyValueArg<T extends string = string>(input: string, options?: RegExpKeyValueArgOptions): KeyValueArg<T> | undefined;
