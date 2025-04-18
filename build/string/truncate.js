2d30757bbd6a6915f695bb84ae2fa2c38e40f9dd
Randal <randal.t.meyer@gmail.com>
2025-04-18 16:03:26 -0700

    easier to follow logic

--- build/string/truncate.js
+++ build/string/truncate.js
@@ -1,6 +1,9 @@
 import { ELLIPSIS } from "./consts.js";
 export function truncate(value, maxLength, ellipsis) {
-    if (value && value.length > maxLength) {
+    if (value === null || value === undefined) {
+        return value;
+    }
+    if (value.length > maxLength) {
         const suffix = ellipsis === true ? ELLIPSIS : ellipsis ?? "";
         const sliced = value.slice(0, maxLength - suffix.length);
         return sliced + suffix;
