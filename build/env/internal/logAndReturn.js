2129a65b8f11f71103d83d2fe90c9515f82f8d0b
Randal <randal.t.meyer@gmail.com>
2025-04-28 16:14:25 -0700

    anonymize all env keys ending with "Token"

--- build/env/internal/logAndReturn.js
+++ build/env/internal/logAndReturn.js
@@ -1,7 +1,7 @@
 import { verbose } from "../../console/loggers/verbose.js";
 import { stringifyJson } from "../../json/stringifyJson.js";
 export function logAndReturn(from, key, value) {
-    const outValue = key === "sageToken" ? value.split("").map(() => "*").join("") : value;
+    const outValue = key.endsWith("Token") ? value.split("").map(() => "*").join("") : value;
     verbose(`Environment Variable (${from}): ${key}=${stringifyJson(outValue)}`);
     return value;
 }
