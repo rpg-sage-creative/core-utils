0035b3d59a82858d75c93af60237e42c60cfcf38
Randal <randal.t.meyer@gmail.com>
2025-06-16 17:04:54 -0700

    fix typo: fedding to feeding

--- src/regex/internal/copyFlags.ts
+++ src/regex/internal/copyFlags.ts
@@ -1,4 +1,4 @@
-/** @internal Removes the v, u, x, and n flags from the RegExp before fedding flags to regex(flags)`` */
+/** @internal Removes the v, u, x, and n flags from the RegExp before feeding flags to regex(flags)`` */
 export function copyFlags(regexp: RegExp): string {
 	return regexp.flags.replace(/[vuxn]/g, "");
 }
