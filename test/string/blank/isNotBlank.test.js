9bf8698f2a113b96caafe0396049430a891eeb12
Randal <randal.t.meyer@gmail.com>
2025-07-01 09:29:16 -0700

    update to use tagLiterals

--- test/string/blank/isNotBlank.test.js
+++ test/string/blank/isNotBlank.test.js
@@ -1,21 +1,20 @@
-import { isNotBlank } from "../../../build/index.js";
-import { toString } from "../../toString.mjs";
+import { isNotBlank, tagLiterals } from "../../../build/index.js";
 
 describe("string", () => {
 	describe("blank", () => {
 		describe("isNotBlank", () => {
 
 			const blankTests = [null, undefined, "", "      ", "\n", "\t", "\n\t", "\n \t"];
-			blankTests.forEach(s => {
-				test(`isNotBlank(${toString(s)}) === false`, () => {
-					expect(isNotBlank(s)).toBe(false);
+			blankTests.forEach(input => {
+				test(tagLiterals`isNotBlank(${input}) === false`, () => {
+					expect(isNotBlank(input)).toBe(false);
 				});
 			});
 
 			const nonBlankTests = ["bob", "."];
-			nonBlankTests.forEach(s => {
-				test(`isNotBlank(${toString(s)}) === true`, () => {
-					expect(isNotBlank(s)).toBe(true);
+			nonBlankTests.forEach(input => {
+				test(tagLiterals`isNotBlank(${input}) === true`, () => {
+					expect(isNotBlank(input)).toBe(true);
 				});
 			});
 
