1a9d5880689c780c18f0bc173dc262f7e1805e02
Randal <randal.t.meyer@gmail.com>
2025-07-16 15:39:59 -0700

    reuse regex

--- build/uuid/parseUuid.js
+++ build/uuid/parseUuid.js
@@ -1,5 +1,5 @@
+const uuidRegex = /(?<uuid>[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)/i;
 export function parseUuid(value) {
-    const regex = /(?<uuid>[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)/i;
-    const match = regex.exec(value ?? "");
+    const match = uuidRegex.exec(value ?? "");
     return match?.groups?.uuid ?? undefined;
 }
