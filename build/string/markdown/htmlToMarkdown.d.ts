c9f15a7e3d3fc4093cb3d0a95dc9c9f875451cfa
Randal <randal.t.meyer@gmail.com>
2025-08-29 15:09:29 -0700

    use InterpolatedValue instead of Pattern

--- build/string/markdown/htmlToMarkdown.d.ts
+++ build/string/markdown/htmlToMarkdown.d.ts
@@ -1,4 +1,4 @@
-import { type Pattern } from "regex";
+import { type InterpolatedValue } from "regex";
 type HtmlToMarkdownHandler = (innerHtml: string, attributes: Map<string, string>, nodeName: Lowercase<string>, outerHtml: string) => string;
-export declare function htmlToMarkdown(text: string, element: string | Pattern, handler: HtmlToMarkdownHandler): string;
+export declare function htmlToMarkdown(text: string, element: InterpolatedValue, handler: HtmlToMarkdownHandler): string;
 export {};
--- src/string/markdown/htmlToMarkdown.ts
+++ src/string/markdown/htmlToMarkdown.ts
@@ -1,4 +1,4 @@
-import { regex, type Pattern } from "regex";
+import { regex, type InterpolatedValue } from "regex";
 import { parseKeyValueArgs } from "../../args/parseKeyValueArgs.js";
 
 /*
@@ -12,9 +12,9 @@ import { parseKeyValueArgs } from "../../args/parseKeyValueArgs.js";
 type HtmlToMarkdownHandler = (innerHtml: string, attributes: Map<string, string>, nodeName: Lowercase<string>, outerHtml: string) => string;
 
 /** @internal Handles nested html tags */
-export function htmlToMarkdown(text: string, element: string | Pattern, openMarkdown: string): string;
-export function htmlToMarkdown(text: string, element: string | Pattern, handler: HtmlToMarkdownHandler): string;
-export function htmlToMarkdown(text: string, element: string | Pattern, handlerOrOpenMarkdown: string | HtmlToMarkdownHandler): string {
+export function htmlToMarkdown(text: string, element: InterpolatedValue, openMarkdown: string): string;
+export function htmlToMarkdown(text: string, element: InterpolatedValue, handler: HtmlToMarkdownHandler): string;
+export function htmlToMarkdown(text: string, element: InterpolatedValue, handlerOrOpenMarkdown: string | HtmlToMarkdownHandler): string {
 	// if we don't have text to convert, just return what we got
 	if (!text) {
 		return text;
