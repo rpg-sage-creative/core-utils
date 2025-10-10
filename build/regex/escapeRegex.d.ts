8527b6ab07a2af2a1778c8819176c454e89c8791
Randal <randal.t.meyer@gmail.com>
2025-10-10 08:53:26 -0700

    update escapeRegex to mimic upcoming RegExp.escape()

--- build/regex/escapeRegex.d.ts
+++ build/regex/escapeRegex.d.ts
@@ -1,11 +1,6 @@
-type Options = {
-    charClass?: boolean;
-    vFlag?: "v";
-};
 /**
  * Escapes RegExp special characters in the given value.
- * If charClass is true, then "-" will be escaped as well.
- * if charClass is true and vFlag is "v", then "!#%&,:;<=>@`~" characters will be escaped, too.
+ * This is a shim until RegExp.escape is available.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/escape
  */
-export declare function escapeRegex(value: string, options?: Options): string;
-export {};
+export declare function escapeRegex(value: string): string;
--- src/regex/escapeRegex.ts
+++ src/regex/escapeRegex.ts
@@ -1,27 +1,22 @@
-type Options = {
-	charClass?: boolean;
-	vFlag?: "v";
-};
-
-/*
-
-\$, \(, \), \*, \+, \., \/, \?, \[, \\, \], \^, \{, \|, \}: valid everywhere
-
-\-: only valid inside character classes
-
-\!, \#, \%, \&, \,, \:, \;, \<, \=, \>, \@, \`, \~: only valid inside v-mode character classes
-
-*/
 
 /**
  * Escapes RegExp special characters in the given value.
- * If charClass is true, then "-" will be escaped as well.
- * if charClass is true and vFlag is "v", then "!#%&,:;<=>@`~" characters will be escaped, too.
+ * This is a shim until RegExp.escape is available.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/escape
  */
-export function escapeRegex(value: string, options?: Options): string {
-	const global = "$()*+./?[]\\^{}|";
-	const charClass = options?.charClass ? "-" : "";
-	const vFlag = options?.charClass && options.vFlag ? "!#%&,:;<=>@`~" : "";
-	const chars = global + charClass + vFlag;
-	return value.split("").map(s => chars.includes(s) ? `\\${s}` : s).join("");
+export function escapeRegex(value: string): string {
+	return value.split("").map((char, index) => {
+		if (!index && /[0-9a-zA-Z]/.test(char) || ",-=<>#&!%:;@~'`\" ".includes(char)) {
+			return `\\x${char.charCodeAt(0).toString(16)}`;
+		}
+		if ("^$\\.*+?()[]{}|/".includes(char)) {
+			return `\\${char}`;
+		}
+		if (char === "\f") return "\\f";
+		if (char === "\n") return "\\n";
+		if (char === "\r") return "\\r";
+		if (char === "\t") return "\\t";
+		if (char === "\v") return "\\v";
+		return char;
+	}).join("");
 }
