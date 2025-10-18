0537107ed08d990305e1ad2159ac68010867c883
Randal <randal.t.meyer@gmail.com>
2025-10-17 17:10:20 -0700

    add OptionalWhitespaceRegExp and OptionalHorizontalWhitespaceRegExp

--- src/string/whitespace/getWhitespaceRegex.ts
+++ src/string/whitespace/getWhitespaceRegex.ts
@@ -1,5 +1,7 @@
 
 export const WhitespaceRegExp = /\s+/;
+export const OptionalWhitespaceRegExp = /\s*/;
 export const WhitespaceRegExpG = new RegExp(WhitespaceRegExp, "g");
 export const HorizontalWhitespaceRegExp = /[^\S\r\n]+/;
+export const OptionalHorizontalWhitespaceRegExp = /[^\S\r\n]*/;
 export const HorizontalWhitespaceRegExpG = new RegExp(HorizontalWhitespaceRegExp, "g");
