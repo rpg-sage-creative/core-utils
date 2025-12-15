74adf4e82a59c67e8b19de733e6890fad1ba35b1
Randal <randal.t.meyer@gmail.com>
2025-12-15 11:42:24 -0800

    navigate and test args one at a time

--- src/string/stringFormat.ts
+++ src/string/stringFormat.ts
@@ -4,8 +4,8 @@ import { navigateJson } from "../json/navigateJson.js";
  * Splits the key into dot notation and looks through the args until it finds a value that matches the key.
  */
 function findByKeyPath(args: any[], key: string): string | undefined {
-	const navResults = args.map(arg => navigateJson(arg, key));
-	for (const navResult of navResults) {
+	for (const arg of args) {
+		const navResult = navigateJson(arg, key);
 		if (navResult.isFull) {
 			return navResult.value;
 		}
@@ -13,7 +13,7 @@ function findByKeyPath(args: any[], key: string): string | undefined {
 	return undefined;
 }
 
-let stringFormatRegex: RegExp;
+const StringFormatRegExp = /\$\{[\w\.\[\]]+}|#\{\d+}/g;
 
 /**
  * Formats the given string template by using the given arguments.
@@ -28,10 +28,8 @@ let stringFormatRegex: RegExp;
 export function stringFormat<T>(template: string, ...args: T[]): string;
 
 export function stringFormat(template: string, ...args: any[]): string {
-	stringFormatRegex ??= /\$\{[\w\.\[\]]+}|#\{\d+}/g;
-
 	const pairs = new Map();
-	return template.replace(stringFormatRegex, keyMatch => {
+	return template.replace(StringFormatRegExp, keyMatch => {
 		if (!pairs.has(keyMatch)) {
 			const key = keyMatch.slice(2, -1);
 			const value = keyMatch.startsWith("$")
