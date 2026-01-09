401488bb2a5cc4e55196d953b0407678b430329f
Randal <randal.t.meyer@gmail.com>
2026-01-09 09:33:28 -0800

    rework testing of sorted arrays with dates

--- test/array/sort/sortPrimitive.test.js
+++ test/array/sort/sortPrimitive.test.js
@@ -1,4 +1,5 @@
 import { sortPrimitive } from "../../../build/index.js";
+import { isDate } from "node:util/types";
 describe("array", () => {
 	describe("sort", () => {
 		describe("sortPrimitive", () => {
@@ -6,66 +7,79 @@ describe("array", () => {
 			const getDatesInput = () => [283334400001, new Date(283334400000), 283334400000, new Date(283334400001), new Date(283334400000)];
 			const getMixedInput = () => getInput().concat(getDatesInput());
 
+			// due to an issue in the GitHubAction workflow, expect(input).toStrictEqual(expected) is having issues with Date values
+			const expectArray = (input, expected) => {
+				expect(input.length).toBe(expected.length);
+				input.forEach((value, index) => {
+					const other = expected[index];
+					if (isDate(value) && isDate(other)) {
+						expect(value.getTime()).toBe(other.getTime());
+					}else {
+						expect(value).toBe(other);
+					}
+				});
+			};
+
 			test(`sortPrimitive`, () => {
 				const input = getInput();
 				const expected = [false, true, true, 1, 1, "1", 2, 2, "2", null, null, null, undefined];
 				expect(input).not.toStrictEqual(expected);
 				input.sort(sortPrimitive);
-				expect(input).toStrictEqual(expected);
+				expectArray(input, expected);
 			});
 
 			test(`sortPrimitive() (reversed)`, () => {
 				const input = getInput();
 				const expected = [undefined, null, null, null, "2", 2, 2, "1", 1, 1, true, true, false];
-				expect(input).not.toEqual(expected);
+				expect(input).not.toStrictEqual(expected);
 				input.sort(sortPrimitive).reverse();
-				expect(input).toEqual(expected);
+				expectArray(input, expected);
 			});
 
 			test(`sortPrimitive (dates)`, () => {
 				const input = getDatesInput();
 				const expected = [new Date(283334400000), new Date(283334400000), 283334400000, new Date(283334400001), 283334400001];
-				expect(input).not.toEqual(expected);
+				expect(input).not.toStrictEqual(expected);
 				input.sort(sortPrimitive);
-				expect(input).toEqual(expected);
+				expectArray(input, expected);
 			});
 
 			test(`sortPrimitive (reversed dates)`, () => {
 				const input = getDatesInput();
 				const expected = [283334400001, new Date(283334400001), 283334400000, new Date(283334400000), new Date(283334400000)];
-				expect(input).not.toEqual(expected);
+				expect(input).not.toStrictEqual(expected);
 				input.sort(sortPrimitive).reverse();
-				expect(input).toEqual(expected);
+				expectArray(input, expected);
 			});
 
 			test(`sortPrimitive (mixed)`, () => {
 				const input = getMixedInput();
 				const expected = [false, true, true, 1, 1, "1", 2, 2, "2", new Date(1978, 11, 24), new Date(1978, 11, 24), 283334400000, new Date(283334400001), 283334400001, null, null, null, undefined];
-				expect(input).not.toEqual(expected);
+				expect(input).not.toStrictEqual(expected);
 				input.sort(sortPrimitive);
-				expect(input).toEqual(expected);
+				expectArray(input, expected);
 			});
 
 			test(`sortPrimitive (reversed mixed)`, () => {
 				const input = getMixedInput();
 				const expected = [undefined, null, null, null, 283334400001, new Date(283334400001), 283334400000, new Date(1978, 11, 24), new Date(1978, 11, 24), "2", 2, 2, "1", 1, 1, true, true, false];
-				expect(input).not.toEqual(expected);
+				expect(input).not.toStrictEqual(expected);
 				input.sort(sortPrimitive).reverse();
-				expect(input).toEqual(expected);
+				expectArray(input, expected);
 			});
 
 			test(`sortPrimitive (single value)`, () => {
 				const input = [false];
 				const expected = [false];
 				input.sort(sortPrimitive).reverse();
-				expect(input).toEqual(expected);
+				expectArray(input, expected);
 			});
 
 			test(`sortPrimitive (empty array)`, () => {
 				const input = [];
 				const expected = [];
 				input.sort(sortPrimitive).reverse();
-				expect(input).toEqual(expected);
+				expectArray(input, expected);
 			});
 		});
 	});
