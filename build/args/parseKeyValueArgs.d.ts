2562d94eea69b1bc70c1a5770ac8970db1bc6afb
Randal <randal.t.meyer@gmail.com>
2026-01-19 00:02:34 -0800

    updated build definitions

--- build/args/parseKeyValueArgs.d.ts
+++ build/args/parseKeyValueArgs.d.ts
@@ -1,8 +1,6 @@
 import type { Optional } from "../types/generics.js";
-import type { TypedRegExp } from "../types/TypedRegExp.js";
-import { type KeyValueArgMatchGroups } from "./parseKeyValueArg.js";
 import type { KeyValueArg } from "./types.js";
-export declare const KeyValueArgRegExpG: TypedRegExp<KeyValueArgMatchGroups, string, string | undefined>;
+export declare const KeyValueArgRegExpG: import("../index.js").TypedRegExp<import("./parseKeyValueArg.js").KeyValueArgMatchGroups, string, string | undefined>;
 /** Returns an array of KeyValueArg values found in the given string. */
 export declare function parseKeyValueArgs<ValueType extends string = string>(input: Optional<string>): KeyValueArg<string, ValueType>[];
 export declare function parseKeyValueArgs<KeyType extends string = string, ValueType extends string = string>(input: Optional<string>): KeyValueArg<KeyType, ValueType>[];
--- build/regex/globalizeRegex.d.ts
+++ build/regex/globalizeRegex.d.ts
@@ -1,5 +1,7 @@
+import type { TypedRegExp } from "../types/TypedRegExp.js";
 /**
  * Returns a duplicate of the given RegExp with the "g" flag added (if it was absent).
  * Can be used quickly clone a "g" RegExp when a unique instance is needed to avoid lastIndex issues.
  */
+export declare function globalizeRegex<Groups extends Record<Keys, Values>, Keys extends string = string, Values extends string | undefined = string | undefined>(regexp: TypedRegExp<Groups, Keys, Values>): TypedRegExp<Groups, Keys, Values>;
 export declare function globalizeRegex<T extends RegExp>(regexp: RegExp): T;
