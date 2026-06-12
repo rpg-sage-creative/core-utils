24196758323c4d232f52d152af2eabfd04174479
Randal <randal.t.meyer@gmail.com>
2026-06-12 09:02:01 -0700

    patch; fix function definition about odd characters to actually use odd characters

--- build/string/wrap/splitWrapChars.d.ts
+++ build/string/wrap/splitWrapChars.d.ts
@@ -5,7 +5,7 @@ type WrapChars = {
 /**
  * Splits the chars into left and right, primarily for use when wrapping text in pairs such as (), ||, and the like.
  * If the chars argument is even, then they are split evenly and used as left/right, such as "()" becoming `{ left:"(", right:")" }`.
- * If the chars argument is odd, then they are used as left and then they are reversed and used as right, such as "_*" becoming `{ left:"_*", right:"*_" }`.
+ * If the chars argument is odd, then they are used as left and then they are reversed and used as right, such as "_*~" becoming `{ left:"_*~", right:"~*_" }`.
  */
 export declare function splitWrapChars(chars: string): WrapChars;
 /** @deprecated renamed to splitWrapChars */
